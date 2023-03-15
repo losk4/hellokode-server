7aebc693b735f49f2c334e4c582054d20845c893
losk4 <54072217+losk4@users.noreply.github.com>
2023-03-16 00:03:09 +0100

    Initial Setup

---
+++ src/main/java/com/loska/hellokode/HellokodeApplication.java
@@ -0,0 +1,13 @@
+package com.loska.hellokode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellokodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellokodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loska/hellokode/HellokodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loska.hellokode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellokodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
